e0fb944230d687e60083e9cda901a062522fac31
Deyvid Joseph Guevara Cueva <deyvid.guevara@inventiva-solutions.com>
2025-11-17 15:36:41 -0500

    feat: Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
+
 import { defineConfig } from 'vite';
-import angular from '@angular-devkit/build-angular/plugins/vite';
 
-const allowedHosts = ['localhost', '127.0.0.1', '::1', 'accura-frontend.azurewebsites.net'];
-
-export default defineConfig(() => ({
-  plugins: [angular()],
+export default defineConfig({
   server: {
-    allowedHosts
-  }
-}));
+    host: '0.0.0.0',
+    port: 8080,
+    allowedHosts: ['accura-frontend.azurewebsites.net','localhost', '127.0.0.1', '::1'],
+  },
+});
