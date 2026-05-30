933238d0a6c662f02554ed1e64d0e1dea3932fa3
Deyvid Joseph Guevara Cueva <deyvid.guevara@inventiva-solutions.com>
2026-05-30 17:51:20 -0500

    fix: actualización de la url de la api en el env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 import { ensureSecureProtocol } from '../app/core/utils/url.utils';
 
-const API_BASE_URL = ensureSecureProtocol('https://accura-api.azurewebsites.net/');
-const NOTIFICATIONS_WS_URL = ensureSecureProtocol('wss://accura-api.azurewebsites.net/notifications/ws');
+const API_BASE_URL = ensureSecureProtocol('https://api-accura.azurewebsites.net/');
+const NOTIFICATIONS_WS_URL = ensureSecureProtocol('wss://api-accura.azurewebsites.net/notifications/ws');
 
 export const environment = {
   production: false,
